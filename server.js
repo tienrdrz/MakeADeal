74e7ed6cb1fdddd27925422a6f8e9816ab6ecff9
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-10-03 20:17:27 -0400

    folders

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'))
+
+app.listen(PORT, () => console.log(`Connected to the Port = ${PORT}`));
