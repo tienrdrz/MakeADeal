e30c6c82bc092de993abeba6cb11b6b46ed997da
Dawson Tilley <16dit@queenus.ca>
2022-10-05 20:02:34 -0400

    homeage skeleton, footer, header

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+}from '@apollo/client';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import { from } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <ApolloProvider client={client}>
+      <Router>
+
+      <div className="App">
+            <Header />
+            <div className='container'>
+
+
+              <Routes>
+                <Route
+                path="/"
+                element={<Home />}
+                />
+              </Routes>
+            </div>
+
+            <Footer />
+        </div>
+        </Router>
+    </ApolloProvider>
+   
   );
 }
 
