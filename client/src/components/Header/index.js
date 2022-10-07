1aa5d938ff049e84a02b5538c9bc5ba4f965e70f
yang <joey674901205@gmail.com>
2022-10-07 13:41:10 -0400

    comment, can render Home page, add npm react-router-dom

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,39 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Auth from '../../utils';
+import Auth from "../../utils/auth";
 
 const Header = () => {
-    const logout = event => {
-        event.preventDefault();
-        Auth.logout();
-    };
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
 
-    return(
-        <header className='Header'>
-            <div className='container'>
-                <Link to="/">
-                <h1>Trading Exchange</h1>
-                </Link>
-                <nav className='nav'>
-                    {Auth.loggedIn()? (
-                       <>
-                       <Link to="/profile">Your Listings
-                       </Link>
-                       <a href="/" onClick={logout}>
-                         Logout
-                       </a>
-                     </>
-                   ) : (
-                     <>
-                       <Link to="/login">Login</Link>
-                       <Link to="/signup">Signup</Link>
-                     </>
-                   )}
-                 </nav>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="Header">
+      <div className="container">
+        <Link to="/">
+          <h1>Trading Exchange</h1>
+        </Link>
+        <nav className="nav">
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Your Listings</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
+
+// import React from "react";
+// import Auth from "../../utils/auth";
+// import { Link } from "react-router-dom";
+
+// function Nav() {
+//   function showNavigation() {
+//     if (Auth.loggedIn()) {
+//       return (
+//         <ul className="">
+//           <li className="">
+//             <a href="/" onClick={() => Auth.logout()}>
+//               logout
+//             </a>
+//           </li>
+//         </ul>
+//       );
+//     } else {
+//       return (
+//         <ul className="">
+//           <li className="">
+//             <Link to="/signup">Signup</Link>
+//           </li>
+//           <li className="">
+//             <Link to="/login">Login</Link>
+//           </li>
+//         </ul>
+//       );
+//     }
+//   }
+
+//   return (
+//     <header className="">
+//       <h1>
+//         <Link to="/">TradeWay</Link>
+//       </h1>
+
+//       <nav>{showNavigation()}</nav>
+//     </header>
+//   );
+// }
+
+// export default Nav;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,23 @@
-import React from 'react';
+import React from "react";
 
-
-import Auth from '../utils/auth';
-
+import Auth from "../utils/auth";
 
 const Home = () => {
-
   const loggedIn = Auth.loggedIn();
 
   return (
     <main>
-      <div className="">
-        {loggedIn && (
-          <div className="">
-            
-          </div>
-        )}
-    
-        {loggedIn && userData ? (
-          <div className="">
-            
-          </div>
-        ) : null}
-      </div>
+      {/* <div className="">
+        {loggedIn && <div className=""></div>}
+
+        {loggedIn && userData ? <div className=""></div> : null}
+      </div> */}
     </main>
+
+    // <div className="">
+    //   <Menu />
+    //   <ItemList />
+    // </div>
   );
 };
 
