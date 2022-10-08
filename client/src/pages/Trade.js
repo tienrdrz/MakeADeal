905b38ce4e38cad2414b7ebdad9ab8503e725f0f
etienne <etiennerodriguez3@gmail.com>
2022-10-08 11:23:14 -0400

    rough trade page, with example items added

--- client/src/pages/Trade.js
+++ client/src/pages/Trade.js
@@ -4,7 +4,38 @@ import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 
 const Trade = () => {
-
+    const items = [
+        {
+            itemName: "Hoodie",
+            itemDesc: "A large black hoodie that is partially used, looks and feels brand new",
+            itemPrice: 35.50,
+            itemImage: "https://t4.ftcdn.net/jpg/04/26/42/63/360_F_426426355_CV69FJfQ2XRWAQtBtOBwkrZwMI0UxVZ0.jpg"
+        },
+        {
+            itemName: "Scarf",
+            itemDesc: "Unused orange ",
+            itemPrice: 35.50,
+            itemImage: "https://t4.ftcdn.net/jpg/04/26/42/63/360_F_426426355_CV69FJfQ2XRWAQtBtOBwkrZwMI0UxVZ0.jpg"
+        }
+    ]
+    return (
+        <main name='trade'>
+            <div className='trade-container'>
+                <h1>Trade</h1>
+                <p>Here you can trade your items with other items listed</p>
+                <div className='trade-items'>
+                    {items.map(({ itemName, itemDesc, itemPrice, itemImage }) => (
+                        <div>
+                            <h3>{itemName}</h3>
+                            <img className="item-image" src={itemImage} alt='' width="304" height="228"/>
+                            <h4>{itemPrice}</h4>
+                            <p>{itemDesc}</p> 
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default Trade;
