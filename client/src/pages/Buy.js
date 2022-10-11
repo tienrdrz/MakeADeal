2a51581c705d59f7304efa264a1b0050b90962a9
yang <joey674901205@gmail.com>
2022-10-11 16:10:00 -0400

    create singleItem page

--- client/src/pages/Buy.js
+++ client/src/pages/Buy.js
@@ -1,11 +1,50 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
+// import { useMutation } from '@apollo/client';
+// import Auth from '../utils/auth';
+
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_ITEM } from "../utils/queries";
 
 const Buy = () => {
-    return (
-        <div></div>
-    )
-}
+  const { id } = useParams();
+
+  const [currentProduct, setCurrentProduct] = useState({});
+
+  const { data } = useQuery(QUERY_ITEM);
+
+  const products = data?.products || [];
+
+  useEffect(() => {
+    if (products.length) {
+      setCurrentProduct(products.find((product) => product._id === id));
+    }
+  }, [products, id]);
+
+  return (
+    <>
+      {currentProduct ? (
+        <div className="">
+          <h2>{currentProduct.name}</h2>
+
+          <p>{currentProduct.description}</p>
+
+          <p>
+            <strong>Price:</strong>${currentProduct.price}{" "}
+            <button>Add to Cart</button>
+            <button>Remove from Cart</button>
+          </p>
+
+          <img
+            src={`/images/${currentProduct.image}`}
+            alt={currentProduct.name}
+          />
+        </div>
+      ) : (
+        <h2>No item</h2>
+      )}
+    </>
+  );
+};
 
 export default Buy;
