d9111fd89044d1220e5aa7e102a975270b268347
etienne <etiennerodriguez3@gmail.com>
2022-10-12 18:47:53 -0400

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/TradeWay",
-  (err) => {
-    if (err) throw err;
-    console.log("connected to MongoDB");
-  }
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/makeadeal123',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
 );
 
 mongoose.set("debug", true);
