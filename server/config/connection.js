ef5910175ad1c62e5d2cdbf5e5e9e37d1abb79d8
yang <joey674901205@gmail.com>
2022-10-12 18:18:17 -0400

    fixed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/TradeWay",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/TradeWay",
   (err) => {
     if (err) throw err;
     console.log("connected to MongoDB");
   }
 );
 
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
 module.exports = mongoose.connection;
